6d7e8dbbd255c63402f5ee8c13b13fa41fc5ef91
Bo-Young PARK <iosdeveloperkr@gmail.com>
2018-01-03 18:18:18 +0900

    [추가] 기찍N  (개선필요)
    //TODO:- 두호님 Tip. variable 따로 선언 말고 input - number 방식으로 변경

---
+++ Algorithms/reversPrintN_#2742.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*:
+ # 기찍 N (#2742)
+ ## 문제
+ 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+ 
+ ## 예
+ ### input:
+ 5
+ ### output:
+ 5 4 3 2 1
+ */
+
+func nTnirp(_ inputNumber:Int) {
+    var result:Int = inputNumber
+    for number in 1...inputNumber {
+        print(result)
+        result -= 1
+    }
+}
+
+nTnirp(5)
+
+//TODO:- 두호님 Tip. variable 따로 선언 말고 input - number 방식으로 변경
+
