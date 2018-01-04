d4b7d9771974f458e48fa733c43387352859f7e3
Bo-Young PARK <iosdeveloperkr@gmail.com>
2018-01-04 11:13:59 +0900

    [추가] OX퀴즈 (아예 못품)
    //TODO:- 두호님 Tip. 재귀, 함수 나누기, Boolean 등을 이용해서 다시 시도해볼 것!

---
+++ Algorithms/OXQuiz_#8958.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*:
+ # OX퀴즈 (#8958)
+ ## 문제
+ "OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
+ 
+ "OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+ 
+ OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
+ 
+ ## 예
+ ### input
+ OOXXOXXOOO
+ 
+ ### output
+ 10
+ */
+
+//FIXME:- 못 풀었습니다.
+func getScoreOfOXQuiz(_ input:String) {
+    let characters = Array(input)
+    var score:Int = 0
+    
+}
+
+//getScoreOfOXQuiz("OXOXOXOOO")
+
+//TODO:- 두호님 Tip. 재귀, 함수 나누기, Boolean 등을 이용해서 다시 시도해볼 것!
