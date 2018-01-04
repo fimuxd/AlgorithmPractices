e4be403d7ebdc62ed61648291573b96d2b9fd512
Bo-Young PARK <iosdeveloperkr@gmail.com>
2018-01-04 11:16:03 +0900

    [추가] 크로아티아 알파벳 (아예 못품)

---
+++ Algorithms/CroatiaAlpabet_#2941.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*:
+ # 크로아티아 알파벳 (#2941)
+ ## 문제
+ 예전에는 운영체제에서 크로아티아 알파벳을 입력할 수가 없었다. 따라서, 다음과 같이 크로아티아 알파벳을 다음과 같이 변경해서 입력했다.
+ 
+ 크로아티아 알파벳    변경
+
+ č    c=
+ 
+ ć    c-
+ 
+ dž    dz=
+ 
+ ñ    d-
+ 
+ lj    lj
+ 
+ nj    nj
+ 
+ š    s=
+ 
+ ž    z=
+ 
+ 예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다.
+ 
+ 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+
+ ## 예
+ ### input
+ ljes=njak
+ 
+ ### output
+ 6
+ 
+ */
+
+//FIXME:- 못 풀었습니다.
+func howManyCroatiaAlphabet(input:String) {
+    var croatiaKey:[String] = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+    
+}
