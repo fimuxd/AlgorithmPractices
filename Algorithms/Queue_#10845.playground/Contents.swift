e2f3eb9e3fe5f74fcbf36839194843735648d147
fimuxd@gmail.com <fimuxd@gmail.com>
2018-01-10 21:35:56 +0900

    [추가] 큐

--- Algorithms/Queue_#10845.playground/Contents.swift
+++ Algorithms/Queue_#10845.playground/Contents.swift
@@ -9,12 +9,12 @@ import UIKit
  
  명령은 총 여섯 가지이다.
  
- * push X: 정수 X를 큐에 넣는 연산이다.
- * pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
- * size: 큐에 들어있는 정수의 개수를 출력한다.
- * empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
- * front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
- * back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+ push X: 정수 X를 큐에 넣는 연산이다.
+ pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+ size: 큐에 들어있는 정수의 개수를 출력한다.
+ empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
+ front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+ back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
  
  ## 예
  ### input
@@ -74,3 +74,64 @@ import UIKit
  3
  */
 
+struct Queue {
+    var items = [Int]()
+    
+    mutating func push(_ item:Int) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Int {
+        if items.isEmpty {
+            return -1
+        } else {
+            return items.removeFirst()
+        }
+    }
+    
+    mutating func size() -> Int{
+        return items.count
+    }
+    
+    mutating func empty() -> Int {
+        if items.isEmpty {
+            return 1
+        } else {
+            return 0
+        }
+    }
+    
+    mutating func front() -> Int {
+        if items.isEmpty {
+            return -1
+        } else {
+            return items.first!
+        }
+    }
+    
+    mutating func back() -> Int {
+        if items.isEmpty {
+            return -1
+        } else {
+            return items.last!
+        }
+    }
+}
+
+var queue:Queue = Queue(items: [])
+
+queue.push(1)
+queue.push(2)
+queue.front()
+queue.back()
+queue.size()
+queue.empty()
+queue.pop()
+queue.pop()
+queue.pop()
+queue.size()
+queue.empty()
+queue.pop()
+queue.push(3)
+queue.empty()
+queue.front()
